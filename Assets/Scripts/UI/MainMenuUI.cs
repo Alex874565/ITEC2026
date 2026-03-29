616120d382672971e6fab440d93f8d17b97bd4c7
unknown <kellerroxana30@gmail.com>
2026-03-29 07:46:57 +0300

    irrelevant

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -16,7 +16,7 @@ public class MainMenuUI : MonoBehaviour
         {
             stagger.CloseMenu(() =>
             {
-                //SceneManager.LoadScene("HubScene");
+                SceneManager.LoadScene("SampleScene");
             });
             
             // Hide title simultaneously
